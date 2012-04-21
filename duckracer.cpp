db5c5bf59bf9c9082ac0704fca4d5a1aaf69f416
Fabian Lesniak <fabian.lesniak@student.kit.edu>
2012-04-21 13:05:59 +0200

    menubar, empty tabWidget added

--- duckracer.cpp
+++ duckracer.cpp
@@ -2,10 +2,33 @@
 
 duckracer::duckracer(QWidget *parent) : QMainWindow(parent)
 {
+    setWindowTitle(tr("DuckRacer2"));
 
+    tabWidget = new QTabWidget(this);
+    setCentralWidget(tabWidget);
+    tabWidget->setTabsClosable(true);
+
+    QMenu *menuDuckracer = new QMenu("D&uckRacer",this);
+    QAction *actionConfigure = menuDuckracer->addAction(QIcon::fromTheme("configure"),tr("O&ptionen"));
+    QAction *actionQuit = menuDuckracer->addAction(QIcon::fromTheme("application-exit"),tr("B&eenden"));
+    menuBar()->addMenu(menuDuckracer);
+
+    connect(actionConfigure,SIGNAL(triggered()),SLOT(processConfigure()));
+    connect(actionQuit,SIGNAL(triggered()),SLOT(processQuit()));
 }
 
 duckracer::~duckracer()
 {
-    
+
+}
+
+void duckracer::processConfigure()
+{
+
+}
+
+void duckracer::processQuit()
+{
+    //TODO: Cleanup?
+    close();
 }
--- duckracer.h
+++ duckracer.h
@@ -1,15 +1,21 @@
 #ifndef DUCKRACER_H
 #define DUCKRACER_H
 
-#include <QtGui/QMainWindow>
+#include <QtGui>
 
 class duckracer : public QMainWindow
 {
     Q_OBJECT
-    
 public:
     duckracer(QWidget *parent = 0);
     ~duckracer();
+
+private:
+    QTabWidget *tabWidget;
+
+private slots:
+    void processConfigure();
+    void processQuit();
 };
 
 #endif // DUCKRACER_H
