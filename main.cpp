7deff72a024f33671b9f4fafc09215c69f14cff8
Fabian Lesniak <fabian@renegade.(none)>
2012-05-03 15:26:38 +0200

    added organization name and application name for settings file

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    //Should translate our openFileDialog, but doesn't?
+    app.setApplicationName("DuckRacer2");
+    app.setOrganizationName("Round Table 76");
+
+    //This should translate our openFileDialog, but doesn't?
     QTranslator qtTranslator;
     qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
     app.installTranslator(&qtTranslator);
